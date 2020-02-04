8dee4f607301c2404e5b2e68dc339326d6e1384a
freakinbook <60284863+freakinbook@users.noreply.github.com>
2020-02-04 10:40:03 +0300

    Uploaded files
    From hard drive in their final version

---
+++ ImageDivider/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ImageDivider
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.button2 = new System.Windows.Forms.Button();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.button1.Location = new System.Drawing.Point(76, 18);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Choose files";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.Multiselect = true;
+			// 
+			// button2
+			// 
+			this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.button2.Location = new System.Drawing.Point(56, 78);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(114, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Divide and save";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 1;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.button2, 0, 1);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(227, 120);
+			this.tableLayoutPanel1.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(251, 144);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Name = "Form1";
+			this.Text = "Image Splitter";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+	}
+}
+
---
+++ ImageDivider/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ImageDivider
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			openFileDialog1.ShowDialog();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			Bitmap image;
+			if (openFileDialog1.FileNames == null)
+			{
+				MessageBox.Show("Please select images");
+				return;
+			}
+			int fileCount = openFileDialog1.FileNames.Length * 2;
+			int index = 0;
+			string newFileName = String.Empty;
+			foreach (var fileName in openFileDialog1.FileNames)
+			{
+				image = new Bitmap(fileName);
+				newFileName = fileName;
+				newFileName = newFileName.Remove(newFileName.LastIndexOf('\\'));
+				newFileName = newFileName + "\\Splitter output";
+				RectangleF rect1 = new RectangleF(0, 0, image.Width / 2, image.Height);
+				RectangleF rect2 = new RectangleF(image.Width / 2, 0, image.Width / 2, image.Height);
+				System.Drawing.Imaging.PixelFormat format = image.PixelFormat;
+				Bitmap left = image.Clone(rect1, format);
+				Bitmap right = image.Clone(rect2, format);
+				Directory.CreateDirectory(newFileName);
+				left.Save(newFileName + "\\" + fileCount + ".png");
+				--fileCount;
+				right.Save(newFileName + "\\" + index + ".png");
+				index++;
+			}
+			MessageBox.Show($"All files successfully created.");
+			Process.Start("explorer.exe", newFileName);
+		}
+	}
+}
